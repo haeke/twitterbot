1ddb62b8aec81b220fc67a4577c56da810c71069
Edwin Rivera <waehake@gmail.com>
2017-06-02 23:30:08 -0400

    added a method to search for tweets, includes a query parameter and counter for the number of tweets we want to show

--- src/twittersearch.js
+++ src/twittersearch.js
@@ -12,15 +12,15 @@ var bot = new Twit({
   timeout_ms: 60 * 1000,
 });
 
-// //  search twitter for all tweets contained in the query, a count and date are included
-// bot.get('search/tweets', { q: 'overwatch since:2011-07-11', count: 100 }, function (err, data, response) {
-//   console.log(data);
-// });
-//
-// //get a list of users who follow a certain account
-// bot.get('followers/ids', { screen_name: 'apple' },  function (err, data, response) {
-//   console.log(data);
-// });
+//  search twitter for all tweets contained in the query, a count and date are included
+bot.get('search/tweets', { q: 'overwatch since:2011-07-11', count: 100 }, function (err, data, response) {
+  console.log(data);
+});
+
+//get a list of users who follow a certain account
+bot.get('followers/ids', { screen_name: 'apple' },  function (err, data, response) {
+  console.log(data);
+});
 
 bot.get('search/tweets', {q: 'eSports',
         count: 5}, function(err, data, response) {
